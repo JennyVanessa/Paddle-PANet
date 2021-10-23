a61c1a1a8f3fb0b6a68f547d3dc637206520cc87
simplify23 <956491659@qq.com>
2021-10-23 12:02:08 +0800

    add init

--- models/loss/__init__.py
+++ models/loss/__init__.py
@@ -1,6 +1,12 @@
-from .dice_loss import DiceLoss
+from .acc import acc
 from .builder import build_loss
-from .ohem import ohem_batch
+from .dice_loss import DiceLoss
+from .emb_loss_v1 import EmbLoss_v1
+from .emb_loss_v2 import EmbLoss_v2
 from .iou import iou
+from .ohem import ohem_batch
 
-__all__ = ['DiceLoss']
+__all__ = [
+    'DiceLoss', 'EmbLoss_v1', 'EmbLoss_v2', 'acc', 'iou', 'ohem_batch',
+    'build_loss'
+]
