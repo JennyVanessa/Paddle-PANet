e9bf15ea8d3f3ec70f6f088736e20957d226c955
simplify23 <956491659@qq.com>
2021-10-23 13:36:02 +0800

    acc change

--- models/loss/acc.py
+++ models/loss/acc.py
@@ -1,14 +1,14 @@
-import torch
+import paddle
 
 EPS = 1e-6
 
 
 def acc_single(a, b, mask):
     ind = mask == 1
-    if torch.sum(ind) == 0:
+    if paddle(ind) == 0:
         return 0
     correct = (a[ind] == b[ind]).float()
-    acc = torch.sum(correct) / correct.size(0)
+    acc = paddle.sum(correct) / correct.size(0)
     return acc
 
 
@@ -19,10 +19,10 @@ def acc(a, b, mask, reduce=True):
     b = b.view(batch_size, -1)
     mask = mask.view(batch_size, -1)
 
-    acc = a.new_zeros((batch_size, ), dtype=torch.float32)
+    acc = a.new_zeros((batch_size, ), dtype="float32")
     for i in range(batch_size):
         acc[i] = acc_single(a[i], b[i], mask[i])
 
     if reduce:
-        acc = torch.mean(acc)
+        acc = paddle.mean(acc)
     return acc
