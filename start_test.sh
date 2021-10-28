9208805d233e362ed1a4dcb9e8bdce4b1e518fd3
simplify23 <39580716+simplify23@users.noreply.github.com>
2021-10-28 10:52:57 +0800

    Update start_test.sh

--- start_test.sh
+++ start_test.sh
@@ -1,18 +1,18 @@
-for i in `seq 510 10 600`, ; do
+# for i in `seq 510 10 600`, ; do
 
-    echo "checkpoint "$i": "
+#     echo "checkpoint "$i": "
 
-    python3.7 test.py config/pan/pan_r18_ctw_train.py checkpoints/pan_r18_ctw_train/"checkpoint_"$i"ep.pdparams"
+#     python3.7 test.py config/pan/pan_r18_ctw_train.py checkpoints/pan_r18_ctw_train/"checkpoint_570ep.pdparams"
 
-    rm -f outputs/submit_ctw/results/.ipynb_checkpoints
+#     rm -f outputs/submit_ctw/results/.ipynb_checkpoints
 
-    cd eval/ctw && python eval.py && cd ../..
+#     cd eval/ctw && python eval.py && cd ../..
 
-done
+# done
 
 echo "checkpoint final :"
 
-python3.7 test.py config/pan/pan_r18_ctw_train.py checkpoints/pan_r18_ctw_train/checkpoint.pdparams
+python3.7 test.py config/pan/pan_r18_ctw_train.py checkpoints/pan_r18_ctw_train/checkpoint_570ep.pdparams
 
 rm -f outputs/submit_ctw/results/.ipynb_checkpoints
 
